f984e137a35df5e851c03bf742ed06275b0f4351
AndreyCorpse28090 <280990corpse@gmail.com>
2024-03-05 22:17:19 +0300

    1

---
+++ Date.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Practica
+{
+     internal class Date
+    {
+        public int year;
+        public int month;
+        public int day;
+
+        //свойства класса
+        public int Day
+        {
+            get { return day; }
+            set
+            {
+                if (value < 1 || value > 31)
+                {
+                    throw new ArgumentException("Некорректное значение дня");
+                }
+                day = value;
+            }
+        }
+
+
+        public int Year
+        {
+            get { return year; }
+            set
+            {
+                if (value < 1 || value > 9999)
+                {
+                    throw new ArgumentException("Некорректное значение года");
+                }
+                year = value;
+            }
+        }
+
+        public int Month
+        {
+            get { return month; }
+            set
+            {
+                if (value < 1 || value > 12)
+                {
+                    throw new ArgumentException("Некорректное значение месяца");
+                }
+                month = value;
+            }
+        }
+
+        // конструкторы
+        // Конструктор устанавливающий начальную дату
+        public Date(int day, int month, int year)
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+
+        // Конструктор устанавливающий день и месяц, а значение year – текущий год
+        public Date(int day, int month) : this(day, month, DateTime.Now.Year) { }
+
+        // Конструктор, устанавливающий день и текущие месяц и год
+        public Date(int day) : this(day, DateTime.Now.Month, DateTime.Now.Year) { }
+
+        // Конструктор, устанавливающий текущий день, месяц и год
+        public Date() : this(DateTime.Now.Day, DateTime.Now.Month, DateTime.Now.Year) { }
+
+        //метод перемещения даты на один день вперед 
+        public void Next()
+        {
+            // проверка на последний день месяца
+            if(day < DateTime.DaysInMonth(year, month))
+            {
+                day++;
+            }
+            else
+            {
+                day = 1;
+
+                if(month < 12)
+                {
+                    month++; 
+                }
+                else
+                {
+                    month = 1;
+                    year++;
+                }
+            }
+        }
+
+
+        // метод получения кол-ва времени, прошедшего от даты 01.01.0000 в днях 
+        public int ToDays()
+        {
+            DateTime startDate = new DateTime(1, 1, 1);
+            DateTime currentDate = new DateTime(year, month, day);
+            return (currentDate - startDate).Days;
+        }
+
+        //  Метод для получения разницы между двумя датами в днях
+        public int DateOffset(Date other)
+        { 
+            DateTime thisDate = new DateTime(year, month, day);
+            DateTime otherDate = new DateTime(other.Year, other.Month, other.Day);
+            return (otherDate - thisDate).Days;
+        }
+
+        // Переопределение ToString
+        public override string ToString()
+        {
+            return $"{day:D2}.{month:D2}.{year:D4}";
+        }
+
+    }
+
+}
+
--- Program.cs
+++ Program.cs
@@ -1,24 +1,173 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace C_Practica
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Fraction fr = new Fraction(2, 8);
-            Fraction fr2 = new Fraction(1, 4);
-            //Console.WriteLine(fr.ToString());
-            Console.WriteLine(fr2.ToString());
+            // Задание 1
+            Task1(args);
+
+            // Задание 2
+            int[] arr = new int[5];
+            Random random = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(0, 100);
+            }
+            Task2(arr, random);
+
+            // Задание 3
+            Task3();
 
 
-            Console.WriteLine(fr.Equals(fr)); 
+            // Задание 4
+            Date date1 = new Date(31, 12, 2024);
+            Console.WriteLine($"Создана дата: {date1}");
+
+            date1.Next();
+            Console.WriteLine($"Дата после увеличения на один день: {date1}");
+
+            int daysFromStart = date1.ToDays();
+            Console.WriteLine($"Количество дней  c 01.01.0000: {daysFromStart}");
+
+            Date date2 = new Date(31, 12, 2024);
+            int dateOffset = date1.DateOffset(date2);
+            Console.WriteLine($"Разница между {date1} и {date2} в днях: {dateOffset}");
 
 
+
+        }
+
+        // Пользователь вводит целое неотрицательное число. Программа должна посчитать общее количество цифр в числе, а также количество нулей в этом числе. 
+        static void Task1(string[] args)
+        {
+            Console.WriteLine("Введите неотрицательное целое число:");
+            string input = Console.ReadLine();
+            uint number = 0;
+
+            if (uint.TryParse(input, out number))
+            {
+                int countDigits = 0;
+                int countZeros = 0;
+
+                // Проверяем, если введенное число равно 0
+                if (number == 0)
+                {
+                    countDigits = 1;
+                    countZeros = 1;
+                }
+                else
+                {
+                    while (number > 0)
+                    {
+                        // последняя цифра числа
+                        uint digit = number % 10;
+
+                        countDigits++;
+
+                        if (digit == 0)
+                        {
+                            countZeros++;
+                        }
+
+                        number /= 10;
+                    }
+                }
+
+                Console.WriteLine($"Общее количество цифр в числе: {countDigits}");
+                Console.WriteLine($"Количество нулей в числе: {countZeros}");
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод. Пожалуйста, введите неотрицательное целое число.");
+            }
+        }
+
+
+
+        //Создать одномерный массив случайных чисел. На его основе создать новый одномерный массив, состоящий из сумм соседних элементов исходного массива.
+        //В случае нечетной длины исходного массива последнее число суммируется с нулем. 
+        static void Task2(int[] arr, Random random)
+        {
+            bool isEvenLength = arr.Length % 2 == 0; // если массив нечётный
+            int newLength = 0;
+
+            newLength = arr.Length / 2;
+            if (!isEvenLength) 
+            {
+                newLength++;
+            }
+
+            int[] newArr = new int[newLength];
+
+            for (int i = 0; i < newLength - 1; i++)
+            {
+                newArr[i] = arr[2 * i] + arr[2 * i + 1];
+            }
+
+            if (!isEvenLength)
+            {
+                newArr[newLength - 1] = arr[arr.Length - 1];
+            }
+
+            Console.WriteLine("Исходный массив:");
+            foreach (int num in arr)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.WriteLine("\nНовый массив, состоящий из сумм соседних элементов:");
+            foreach (int num in newArr)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+        }
+
+
+
+    /* 
+    Задание 3. 
+    Создать зубчатый двумерный массив по следующему правилу: 
+    1 
+    1 2 
+    1 2 3 
+    1 2 3 4 
+    1 2 3 4 5 … n 
+    n вводится пользователем. 
+    */
+
+    static void Task3() 
+        {
+            Console.WriteLine("Введите число n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[][] jaggedArray = new int[n][];
+
+            // Заполнение массива
+            for (int i = 0; i < n;i++)
+            {
+                jaggedArray[i] = new int[i + 1];
+
+                for(int j = 0; j <= i; j++)
+                {
+                    jaggedArray[i][j] = j + 1;
+                }
+            }
+
+            Console.WriteLine("Зубчатый двумерный массив:");
+            // вывод в консоль
+            foreach (int[] row in jaggedArray)
+            {
+                foreach (int num in row)
+                {
+                    Console.Write(num);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
